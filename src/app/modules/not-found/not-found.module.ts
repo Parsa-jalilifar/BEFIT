83673426e119b085896e72e874658ee9ed63a7af
Parsa-jalilifar <parsajalilifar@gmail.com>
2021-07-14 18:53:45 -0400

    not found page completed

--- src/app/modules/not-found/not-found.module.ts
+++ src/app/modules/not-found/not-found.module.ts
@@ -2,17 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
 
+import { AppRoutingModule } from 'src/app/app-routing.module';
+
 
 
 @NgModule({
-  declarations: [
-    NotFoundComponent
-  ],
+  declarations: [NotFoundComponent],
   imports: [
-    CommonModule
-  ],
-  exports: [
-    NotFoundComponent
+    CommonModule,
+    AppRoutingModule
   ]
 })
 export class NotFoundModule { }
