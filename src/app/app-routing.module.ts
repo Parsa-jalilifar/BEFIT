78b6895a81d93064b56a05c0d4d1259aff7e757b
Parsa-jalilifar <parsajalilifar@gmail.com>
2021-07-14 16:19:52 -0400

    not-found is added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router';
 
-
-// import { NotFoundComponent } from './components/not-found/not-found.component';
+import { NotFoundComponent } from './modules/not-found/pages/not-found/not-found.component';
+import { HomeComponent } from './modules/home/pages/home/home.component';
 
 
 const routes: Routes = [
-  // { path: "**", component: NotFoundComponent },
-  // { path: "home", component: NotFoundComponent },
+  { path: "", component: HomeComponent },
+  { path: "**", component: NotFoundComponent },
 ];
 
 @NgModule({
