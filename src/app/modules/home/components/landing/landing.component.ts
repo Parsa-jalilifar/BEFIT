d25b3e197c27cad64634cba73176636fa9f3bec9
Parsa-jalilifar <parsajalilifar@gmail.com>
2021-07-15 16:27:19 -0400

    header intersection observer added

--- src/app/modules/home/components/landing/landing.component.ts
+++ src/app/modules/home/components/landing/landing.component.ts
@@ -13,6 +13,27 @@ export class LandingComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    this.intersectionObserver();
+  }
+
+  intersectionObserver(): void {
+
+    const header = document.querySelector(".header");
+    const landing = document.querySelector("#landing");
+
+    const options = {};
+
+    const headerObserver = new IntersectionObserver((entries, menuObserver) => {
+      entries.forEach((entry) => {
+        if (!entry.isIntersecting) {
+          header.classList.add("move");
+        } else {
+          header.classList.remove("move");
+        }
+      });
+    }, options);
+
+    headerObserver.observe(landing);
   }
 
 }
