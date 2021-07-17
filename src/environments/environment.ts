9632125f15fe21aa2b3b1f694d9942976247eba5
Parsa-jalilifar <parsajalilifar@gmail.com>
2021-07-16 20:34:15 -0400

    firebase store added

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,16 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCzDbDUXPsMXmcpwFUWkIhqlUDGNPrQOXc",
+    authDomain: "befit-96585.firebaseapp.com",
+    projectId: "befit-96585",
+    storageBucket: "befit-96585.appspot.com",
+    messagingSenderId: "519373935203",
+    appId: "1:519373935203:web:ff6870a5d5444a46a19e56",
+    measurementId: "G-JLEVFJ495R"
+  }
 };
 
 /*
