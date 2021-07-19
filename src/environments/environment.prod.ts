bfffae86c3148e03dcc2ac542b1003276217c3c8
Parsa-jalilifar <parsajalilifar@gmail.com>
2021-07-18 22:34:05 -0400

    added firebase to environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCzDbDUXPsMXmcpwFUWkIhqlUDGNPrQOXc",
+    authDomain: "befit-96585.firebaseapp.com",
+    projectId: "befit-96585",
+    storageBucket: "befit-96585.appspot.com",
+    messagingSenderId: "519373935203",
+    appId: "1:519373935203:web:ff6870a5d5444a46a19e56",
+    measurementId: "G-JLEVFJ495R"
+  }
 };
